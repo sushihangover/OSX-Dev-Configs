debe08b556e87d407df9307c7b762eea5d64ebeb
sushihangover <sushihangover.outlook.com>
2013-10-28 21:29:44 -0700

    Syncing from/to HG and GIT in same directory
    http://sushihangover.azurewebsites.net/post/Irony-Syncing-fromto-HG-and-GIT-in-same-directory-Part-2

---
+++ sync-commits-hg2git.sh
@@ -0,0 +1,19 @@
+# Sync commit from an HG repo to a Git repo in the same directory
+#
+# http://sushihangover.azurewebsites.net/post/Irony-Syncing-fromto-HG-and-GIT-in-same-directory-Part-2
+# 
+function migratecommits {
+hg log -r : |grep "changeset:   "|cut -f 3 -d : >$1
+hg log|grep changeset|wc -l
+count=0
+while read cset; do
+  printf '%d %s\n' "$count" "$cset"
+  (( count++ ))
+  hg log --rev $cset > /tmp/hg_commit_info.tmp
+  hg update -r $cset -C
+  echo -e "hg2git:      Preserving hg log changeset\n$(cat /tmp/hg_commit_info.tmp)" > /tmp/hg_commit_info.tmp
+  git add --all 
+  tail -r /tmp/hg_commit_info.tmp | git commit -a --file=- # --dry-run
+  hg up -C
+done < $1
+}
